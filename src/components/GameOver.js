4beb19ef8ca11c9d6bf5d003539e68108a915a93
Eric-Formiga <eric.13formiga@gmail.com>
2022-12-14 22:40:29 -0300

    projeto finalizado

---
+++ src/components/GameOver.js
@@ -0,0 +1,13 @@
+import "./GameOver.css"
+
+const GameOver = ({retry, score}) => {
+  return (
+    <div>
+      <h2>Fim de jogo!</h2>
+      <h2>A sua pontuação foi: <span>{score}</span></h2>
+      <button onClick={retry}>Resetar jogo</button>
+    </div>
+  )
+}
+
+export default GameOver
---
+++ src/datas/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro:["motor","capô","porta","pneu","volante"],
+    fruta:["maça","banana","uva","laranja"],
+    corpo:["dedo","braço","perna","olhos"],
+    computador:["memoria","teclado","mouse","gabinete"],
+    programação:["javascript","react","framework","linguagem"],
+    alimento:["arroz","feijão","carne","ovos"]
+}
